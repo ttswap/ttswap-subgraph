b04d534d18f1c58a157d367ce075973660943a36
macbook m3 <ttswap.exchange@gmail.com>
2026-05-06 20:52:57 +0800

    fix some bug 20260506
    Signed-off-by: macbook m3 <ttswap.exchange@gmail.com>

--- src/ttswap_market.ts
+++ src/ttswap_market.ts
@@ -1568,8 +1568,9 @@ export function handle_e_disinvestProof(event: e_disinvestProof): void {
 
 
 
-                value_good.virtualQuantity = value_good.virtualQuantity.minus(event.params._valueprofit.mod(BI_128));
-                value_good.virtualQuantity = value_good.virtualQuantity.plus(event.params._valuedisvest.mod(BI_128));
+                value_good.virtualQuantity = value_good.virtualQuantity.plus(event.params._valueprofit.mod(BI_128));
+                value_good.virtualQuantity = value_good.virtualQuantity.minus(event.params._valuedisvest.mod(BI_128));
+
                 value_good.currentValue = value_good.currentValue.minus(event.params._value.div(BI_128));
 
                 value_good.currentQuantity = value_good.currentQuantity.minus(event.params._valueprofit.mod(BI_128));
@@ -1602,7 +1603,7 @@ export function handle_e_disinvestProof(event: e_disinvestProof): void {
 
                 value_good.totalDisinvestQuantity =
                         value_good.totalDisinvestQuantity.plus(
-                                event.params._valueprofit.mod(BI_128)
+                                event.params._valuedisvest.mod(BI_128)
                         );
                 value_good.totalDisinvestCount =
                         value_good.totalDisinvestCount.plus(ONE_BI);
